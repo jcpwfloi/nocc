642f84e00e519d36661f8d232e564e34b8b55751
Zhongrui Chen <5636554+jcpwfloi@users.noreply.github.com>
2020-05-05 17:09:00 -0400

    Showing grade

--- cc.js
+++ cc.js
@@ -160,6 +160,30 @@ async function selectSemester(body, termNum) {
   });
 }
 
+async function getpid() {
+  var body = await
+    ccRequest("https://cs.cc.unc.edu/psc/campus/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART&Action=A&ExactKeys=Y", {
+      "headers": {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ja;q=0.6,de-DE;q=0.5,de;q=0.4,zh-TW;q=0.3",
+        "sec-fetch-dest": "iframe",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "cookie": config.cookie
+      },
+      "referrerPolicy": "no-referrer-when-downgrade",
+      "body": null,
+      "method": "GET",
+      "mode": "cors"
+    });
+  var pid = /EMPLID=\d+&/.exec(body);
+  pid = pid[0];
+  pid = pid.substr(7, 9);
+  return pid;
+}
+
 async function check(term) {
   await entry();
   var body = await
@@ -197,24 +221,23 @@ async function check(term) {
   return ans;
 }
 
-async function getGrade() {
+async function getGrade(termNum) {
+  var term = termNum || '2209';
+  await entry();
   var body = await
-    ccRequest("https://cs.cc.unc.edu/psc/campus/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL", {
+    ccRequest(`https://cs.cc.unc.edu/psc/campus/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL??Action=A&ExactKeys=Y&ACAD_CAREER=UGRD&EMPLID=${await getpid()}&INSTITUTION=UNCCH&STRM=${term}`, {
       "headers": {
         "accept": "*/*",
         "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ja;q=0.6,de-DE;q=0.5,de;q=0.4,zh-TW;q=0.3",
         "content-type": "application/x-www-form-urlencoded",
         "sec-fetch-dest": "empty",
         "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "cookie": config.cookie
+        "sec-fetch-site": "same-origin"
       },
       "referrerPolicy": "no-referrer-when-downgrade",
-      "body": "ICAJAX=1&ICNAVTYPEDROPDOWN=0&ICType=Panel&ICElementNum=0&ICStateNum=22&ICAction=DERIVED_SSS_SCT_SSR_PB_GO&ICModelCancel=0&ICXPos=0&ICYPos=0&ResponsetoDiffFrame=-1&TargetFrameName=None&FacetPath=None&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICSkipPending=0&ICAutoSave=0&ICResubmit=0&ICSID=xyfeYiNiGoqlQVHcVzvEXrdkxy%2BT24yMMsbYTH3t0e8%3D&ICActionPrompt=false&ICBcDomData=C~UnknownValue~EMPLOYEE~EMPL~NUI_FRAMEWORK.PT_LANDINGPAGE.GBL~PT_LANDINGPAGE~Student~UnknownValue~UnknownValue~https%3A%2F%2Fpa.cc.unc.edu%2Fpsc%2Fpaprd%2FEMPLOYEE%2FEMPL%2Fc%2FNUI_FRAMEWORK.PT_LANDINGPAGE.GBL%3FInfoShibsession%3Dtrue~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL~SSS_STUDENT_CENTER~Student%20Center~UnknownValue~UnknownValue~https%3A%2F%2Fpa.cc.unc.edu%2Fpsp%2Fpaprd%2FEMPLOYEE%2FSA%2Fc%2FSA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL~SSS_STUDENT_CENTER~Student%20Center~UnknownValue~UnknownValue~https%3A%2F%2Fpa.cc.unc.edu%2Fpsp%2Fpaprd%2FEMPLOYEE%2FSA%2Fc%2FSA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL~SSR_SSENRL_TERM~Select%20Term~UnknownValue~UnknownValue~https%3A%2F%2Fpa.cc.unc.edu%2Fpsp%2Fpaprd%2FEMPLOYEE%2FSA%2Fc%2FSA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL%3FAction%3DA%26ExactKeys%3DY~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL~SSR_SSENRL_CART~Shopping%20Cart~UnknownValue~UnknownValue~https%3A%2F%2Fpa.cc.unc.edu%2Fpsp%2Fpaprd%2FEMPLOYEE%2FSA%2Fc%2FSA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL%3FAction%3DA%26ACAD_CAREER%3DUGRD%26EMPLID%3D730245762%26INSTITUTION%3DUNCCH%26STRM%3D2203~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL~SSS_STUDENT_CENTER~Student%20Center~UnknownValue~UnknownValue~https%3A%2F%2Fpa.cc.unc.edu%2Fpsp%2Fpaprd%2FEMPLOYEE%2FSA%2Fc%2FSA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL%3FAction%3DU~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL~SSR_SSENRL_GRADE~Student%20Grades~UnknownValue~UnknownValue~https%3A%2F%2Fpa.cc.unc.edu%2Fpsp%2Fpaprd%2FEMPLOYEE%2FSA%2Fc%2FSA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL%3F~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SA_LEARNER_SERVICES.SSS_MY_PLANNER.GBL~SSS_MY_PLANNER~My%20Planner~UnknownValue~UnknownValue~https%3A%2F%2Fpa.cc.unc.edu%2Fpsp%2Fpaprd%2FEMPLOYEE%2FSA%2Fc%2FSA_LEARNER_SERVICES.SSS_MY_PLANNER.GBL%3FAction%3DA~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SA_LEARNER_SERVICES.SSS_MY_ACAD.GBL~SSS_MY_ACAD~My%20Academics~UnknownValue~UnknownValue~https%3A%2F%2Fpa.cc.unc.edu%2Fpsp%2Fpaprd%2FEMPLOYEE%2FSA%2Fc%2FSA_LEARNER_SERVICES.SSS_MY_ACAD.GBL%3FAction%3DU~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL~SSS_STUDENT_CENTER~Student%20Center~UnknownValue~UnknownValue~https%3A%2F%2Fpa.cc.unc.edu%2Fpsp%2Fpaprd%2FEMPLOYEE%2FSA%2Fc%2FSA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL%3FAction%3DU~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL~SSR_SSENRL_GRADE~Student%20Grades~UnknownValue~UnknownValue~https%3A%2F%2Fpa.cc.unc.edu%2Fpsp%2Fpaprd%2FEMPLOYEE%2FSA%2Fc%2FSA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL%3F~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL~SSS_STUDENT_CENTER~Student%20Center~UnknownValue~UnknownValue~https%3A%2F%2Fpa.cc.unc.edu%2Fpsp%2Fpaprd%2FEMPLOYEE%2FSA%2Fc%2FSA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL~SSR_SSENRL_TERM~Select%20Term~UnknownValue~UnknownValue~https%3A%2F%2Fpa.cc.unc.edu%2Fpsp%2Fpaprd%2FEMPLOYEE%2FSA%2Fc%2FSA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL%3FAction%3DA~UnknownValue&ICPanelName=&ICFind=&ICAddCount=&ICAppClsData=&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$27$=9999&SSR_DUMMY_RECV1$sels$2$$0=2",
-      "method": "POST",
+      "method": "GET",
       "mode": "cors"
     });
-  body = await body.text();
   var $ = cheerio.load(body);
   $('table.PSLEVEL1GRID').first().find('tbody > tr').each(async function() {
     var row = $(this).find('td');
--- grade.js
+++ grade.js
@@ -1,1 +1,8 @@
-require('./cc').getGrade();
+const cc = require('./cc');
+
+async function main() {
+  console.log('Your grade for Spring 2020:');
+  console.log(await cc.getGrade('2202'));
+}
+
+main();
